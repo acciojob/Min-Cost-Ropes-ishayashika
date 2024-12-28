39641b0a626cf73d740eda39a57f0f17a5bdefda
ishayashika <122111684+ishayashika@users.noreply.github.com>
2024-12-28 16:09:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,14 @@ function mincost(arr)
 { 
 //write your code here
 // return the min cost
-  
+let total_cost=0;
+while(arr.length>1){
+	arr.sort((a,b)=>a-b);
+	sum=arr[0]+arr[1];
+	total_cost+=sum;
+	arr=[cost,slice(2)];
+}
+	 return total_cost; 
 }
 
 module.exports=mincost;
